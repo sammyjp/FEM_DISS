80cfb24f809e3afb22787b87653985b97827bb03
sammy.petros@btinternet.com <wh6i4w64im40>
2017-06-18 15:16:14 +0100

    Added in SP_FEM header file to contain all the header files needed.

--- DISSERTATION/FEM_Project/Driver.cpp
+++ DISSERTATION/FEM_Project/Driver.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-#include "Element.hpp"
-#include "FE_Solution.hpp"
-#include "Matrix.hpp"
-#include "Mesh.hpp"
-#include "Quadrature.hpp"
+#include "SP_FEM.hpp"
 
 int main(int argc, char* argv[])
 {
-
-
     return 0;
 }
---
+++ DISSERTATION/FEM_Project/SP_FEM.hpp
@@ -0,0 +1,8 @@
+#ifndef SP_FEMHEADERDEF
+#define SP_FEMHEADERDEF
+
+#include "FE_Solution.hpp"
+#include "Mesh.hpp"
+#include "Quadrature.hpp"
+
+#endif
